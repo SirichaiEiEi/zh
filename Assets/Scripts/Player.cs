9ceb77dd1699b6bd9016d2092ba2959ba497a5f2
ObeslikB <anusorn.b@gmail.com>
2022-08-23 11:30:04 +0700

    update hp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class Player : MonoBehaviour
 {
     public AudioClip firearm;
     AudioSource asource;
+    float hp = 100;
+    Image imgHP;
     
     // Start is called before the first frame update
     void Start()
     {
         asource = GetComponent<AudioSource>();
+        imgHP =  GameObject.Find("HP").GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -18,6 +23,15 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             asource.PlayOneShot(firearm);
+        }
+        imgHP.fillAmount = hp / 100;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zombie"))
+        {
+            hp -= 20;
         }
     }
 }
